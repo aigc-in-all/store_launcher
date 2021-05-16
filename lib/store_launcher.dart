efffb3b8f2bc26d42714bc5346531153fe5eb383
deakjahn <deakjahn@gmail.com>
2021-05-16 12:27:25 +0200

    Removing unused remnants of the plugin boilerplate.

--- lib/store_launcher.dart
+++ lib/store_launcher.dart
@@ -5,11 +5,6 @@ import 'package:flutter/services.dart';
 class StoreLauncher {
   static const MethodChannel _channel = const MethodChannel('store_launcher');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static Future<void> openWithStore(String appId) async {
     await _channel.invokeMethod('openWithStore', {"app_id": appId});
   }
